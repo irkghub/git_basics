75d1914ca003832283da53315744734290def1ca
irkghub <bkbhandari99@gmail.com>
2023-12-03 18:40:45 +0545

    automation script for initial commit

---
+++ automation_script_for_initial_commit/firstpush.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#!/bin/bash
+
+read -p "Enter the name of the directory: " dir_name
+read -p "Enter the remote Git repository URL: " repo_name
+
+echo "Directory name: $dir_name"
+echo "Git repository URL: $repo_name"
+
+mkdir "$dir_name" && cd "$dir_name"
+
+touch README.md
+git init
+git add .
+git commit -m "Initial Commit"
+git branch -M main
+git remote add origin "$repo_name"
+git push -u origin main
+
+
